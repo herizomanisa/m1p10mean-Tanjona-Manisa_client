d39d64466853e89462501ac8f10759f564c9bcdf
Manisa <manisarajemisa24@gmail.com>
2024-02-27 01:21:33 +0300

    feat: create nav client menu

---
+++ src/app/containers/default-layout/_nav_client.ts
@@ -0,0 +1,34 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Liste des services',
+    url: '/',
+    iconComponent: { name: 'cil-calculator' },
+  },
+  {
+    name: 'Liste des employés',
+    url: '/',
+    iconComponent: { name: 'cil-people' },
+  },
+  {
+    name: 'Mes rendez-vous',
+    url: '/',
+    iconComponent: { name: 'cil-calendar' },
+  },
+  {
+    name: 'Préférence',
+    url: '/preference',
+    iconComponent: { name: 'cil-star' },
+    children: [
+      {
+        name: 'Services',
+        url: '/preference/service',
+      },
+      {
+        name: 'Employés',
+        url: '/preference/employe',
+      },
+    ],
+  },
+];
