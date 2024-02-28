baed606a29d90e8de56eca48dbe0f880669e18d8
Manisa <manisarajemisa24@gmail.com>
2024-02-28 09:35:44 +0300

    fix: route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -87,7 +87,7 @@ const routes: Routes = [
     },
     children: [
       {
-        path: 'service',
+        path: '',
         loadChildren: () =>
           import('./views/service/service.module').then((m) => m.ServiceModule),
       },
@@ -155,83 +155,29 @@ const routes: Routes = [
     pathMatch: 'full',
   },
 
-  {
-    path: '',
-    component: DefaultLayoutClientComponent,
-    canActivate: [canActivateClient],
-    data: {
-      expectedRole: 'customer',
-      title: 'Home',
-    },
-    children: [
-      {
-        path: '',
-        loadChildren: () =>
-          import('./views/service/service.module').then((m) => m.ServiceModule),
-      },
-      {
-        path: 'rendezvous-form',
-        loadChildren: () =>
-          import('./views/rendezvous-form/rendezvous-form.module').then(
-            (m) => m.RendezvousFormModule
-          ),
-      },
-      // {
-      //   path: 'dashboard',
-      //   loadChildren: () =>
-      //     import('./views/dashboard/dashboard.module').then(
-      //       (m) => m.DashboardModule
-      //     ),
-      // },
-      // {
-      //   path: 'theme',
-      //   loadChildren: () =>
-      //     import('./views/theme/theme.module').then((m) => m.ThemeModule),
-      // },
-      // {
-      //   path: 'base',
-      //   loadChildren: () =>
-      //     import('./views/base/base.module').then((m) => m.BaseModule),
-      // },
-      // {
-      //   path: 'buttons',
-      //   loadChildren: () =>
-      //     import('./views/buttons/buttons.module').then((m) => m.ButtonsModule),
-      // },
-      // {
-      //   path: 'forms',
-      //   loadChildren: () =>
-      //     import('./views/forms/forms.module').then((m) => m.CoreUIFormsModule),
-      // },
-      // {
-      //   path: 'charts',
-      //   loadChildren: () =>
-      //     import('./views/charts/charts.module').then((m) => m.ChartsModule),
-      // },
-      // {
-      //   path: 'icons',
-      //   loadChildren: () =>
-      //     import('./views/icons/icons.module').then((m) => m.IconsModule),
-      // },
-      // {
-      //   path: 'notifications',
-      //   loadChildren: () =>
-      //     import('./views/notifications/notifications.module').then(
-      //       (m) => m.NotificationsModule
-      //     ),
-      // },
-      // {
-      //   path: 'widgets',
-      //   loadChildren: () =>
-      //     import('./views/widgets/widgets.module').then((m) => m.WidgetsModule),
-      // },
-      // {
-      //   path: 'pages',
-      //   loadChildren: () =>
-      //     import('./views/pages/pages.module').then((m) => m.PagesModule),
-      // },
-    ],
-  },
+  // {
+  //   path: '',
+  //   component: DefaultLayoutClientComponent,
+  //   canActivate: [canActivateClient],
+  //   data: {
+  //     expectedRole: 'customer',
+  //     title: 'Home',
+  //   },
+  //   children: [
+  //     {
+  //       path: '',
+  //       loadChildren: () =>
+  //         import('./views/service/service.module').then((m) => m.ServiceModule),
+  //     },
+  //     {
+  //       path: 'rendezvous-form',
+  //       loadChildren: () =>
+  //         import('./views/rendezvous-form/rendezvous-form.module').then(
+  //           (m) => m.RendezvousFormModule
+  //         ),
+  //     },
+  //   ],
+  // },
   {
     path: '404',
     component: Page404Component,
