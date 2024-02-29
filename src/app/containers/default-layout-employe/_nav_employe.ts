2e1d722ff11c05e176044423b6fd8aaf918bff6e
Manisa <manisarajemisa24@gmail.com>
2024-02-29 20:47:19 +0300

    feat: add menu Rendez vous

--- src/app/containers/default-layout-employe/_nav_employe.ts
+++ src/app/containers/default-layout-employe/_nav_employe.ts
@@ -2,22 +2,27 @@ import { INavData } from '@coreui/angular';
 
 export const navItemsEmploye: INavData[] = [
   {
+    name: 'Rendez-vous',
+    url: '/employe',
+    iconComponent: { name: 'cil-calendar' },
+  },
+  {
     name: 'Mon profil',
     url: '/employe/profil',
     iconComponent: { name: 'cil-people' },
   },
-//   {
-//     name: 'Rendez-vous',
-//     iconComponent: { name: 'cil-calendar' },
-//     children: [
-//       {
-//         name: 'Mes RDV',
-//         url: '/employe/my-list',
-//       },
-//       {
-//         name: 'Non assignés',
-//         url: '/employe/no-employe',
-//       },
-//     ],
-//   },
+  //   {
+  //     name: 'Rendez-vous',
+  //     iconComponent: { name: 'cil-calendar' },
+  //     children: [
+  //       {
+  //         name: 'Mes RDV',
+  //         url: '/employe/my-list',
+  //       },
+  //       {
+  //         name: 'Non assignés',
+  //         url: '/employe/no-employe',
+  //       },
+  //     ],
+  //   },
 ];
